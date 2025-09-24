13dbde3eb32da710dd5fa6b170c5fee097130d4b
Bluz0 <amouats5@gmail.com>
2025-09-24 18:49:25 +0200

    test : bon commit

--- app/src/main/java/td/info507/weatherusmb/MainActivity.kt
+++ app/src/main/java/td/info507/weatherusmb/MainActivity.kt
@@ -1,16 +1,37 @@
 package td.info507.weatherusmb
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import td.info507.weatherusmb.ui.theme.WeatherUsmbTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +40,37 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             WeatherUsmbTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainWeatherScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainWeatherScreen(){
+    val context = LocalContext.current
+    Box(modifier = Modifier.fillMaxSize()){
+        Row(modifier = Modifier.fillMaxWidth().paddingFromBaseline(50.sp),){
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End){
+                IconButton(onClick = {}, modifier = Modifier.padding(20.dp,0.dp).height(70.dp).width(70.dp).background(Color.LightGray)){
+                    Icon(
+                        painter = painterResource(R.drawable.location),
+                        contentDescription = "endroit.xml",
+                        modifier = Modifier.height(70.dp).width(50.dp)
+                    )
+                }
+            }
+
+        }
+    }
+
 }
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun WeatherMainPreview() {
     WeatherUsmbTheme {
-        Greeting("Méteos")
+        MainWeatherScreen()
     }
 }
